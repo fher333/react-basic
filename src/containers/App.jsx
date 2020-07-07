ed627561c72048f8c66c58b505ea82b9814f4aa2
fher333 <fher.melendez@gmail.com>
2020-07-06 20:30:05 -0500

    cierre del curso react basico

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,11 +9,11 @@ import Footer from '../components/Footer';
 
 const App = () => {
   const [ videos, setVideos ] = useState(
-{
-  'mylist':[],
-  'trends':[],
-  'originals':[]
-}
+    {
+      'mylist':[],
+      'trends':[],
+      'originals':[],
+    },
   );
 
   useEffect(() => {
@@ -27,26 +27,25 @@ const App = () => {
     <div className='App'>
       <Header />
       <Search />
-      {videos.mylist.length > 0 &&
+      {videos.mylist.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
             <CarouselItem />
           </Carousel>
         </Categories>
-      }
-      
+      )}
 
       <Categories title='Tendencias'>
         <Carousel>
-        {videos.trends.map((item) =>
-          <CarouselItem key={item.id} {...item} />
-        )}
-          
+          {videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+
         </Carousel>
       </Categories>
 
       <Categories title='Originales'>
         <Carousel>
+          {videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+
           <CarouselItem />
         </Carousel>
       </Categories>
